653c51f6fa3243453de6e4b3f22d1076dd5d40bb
Gabe Lalasava <gabe@fijiwebdesign.com>
2018-04-20 17:10:27 +0800

    Fix abstract

--- FileSessionData.php
+++ FileSessionData.php
@@ -52,9 +52,9 @@ class FileSessionData extends AbstractSessionData
    * $SessionData = FileSessionData::singleton();
    * 
    */
-  public static function singleton(array $args = array(), $classname = __CLASS__)
+  public static function singleton(array $args = array(), $classname = null)
   {
-    return parent::singleton($args, $classname);
+    return parent::singleton($args, __CLASS__);
   }
   
   /**
